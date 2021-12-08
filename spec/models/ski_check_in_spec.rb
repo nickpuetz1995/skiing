2911136119db0844c31911dd16b9ac89c52b062f
anonymous anonymous <nick.puetz@kellogg.northwestern.edu>
2021-12-08 18:34:05 +0000

    Indirect association between friends and ski_check_ins

--- spec/models/ski_check_in_spec.rb
+++ spec/models/ski_check_in_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe SkiCheckIn, type: :model do
 
     describe "InDirect Associations" do
 
+    it { should have_one(:friend_invites_sent) }
+
     end
 
     describe "Validations" do
