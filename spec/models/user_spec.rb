988f5b5f04c46f310246f281d3d89696f8851cad
anonymous anonymous <nick.puetz@kellogg.northwestern.edu>
2021-12-08 18:34:05 +0000

    Direct association between users and ski_area_reviews

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:ski_area_reviews) }
+
     it { should have_many(:ski_check_ins) }
 
     it { should have_many(:friend_requests) }
